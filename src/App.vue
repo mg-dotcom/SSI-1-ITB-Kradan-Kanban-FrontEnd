d5f2402dc7242dbb160451de91ed49f95c3d405f
mg.apisara <111364885+mg-dotcom@users.noreply.github.com>
2024-04-24 16:25:09 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
       <img class="absolute right-0" src="/glass-overlay.png" alt="" />
       <div class="h-[90px] flex flex-col justify-center p-10">
         <h1 class="text-header text-white font-bold">
-          IT-Bangmod Kradan kanban ฟหกฟหก
+          IT-Bangmod Kradan kanban
         </h1>
       </div>
     </div>
@@ -43,6 +43,36 @@
                   </th> -->
                 </tr>
               </thead>
+              <!-- if no data 
+              <tbody class="bg-white divide-y divide-[#CACACA]">
+                <tr class="divide-x divide-[#CACACA]">
+                  <td class="px-6 py-4 whitespace-nowrap">
+                    <!-- <div class="flex items-center">
+                      <div class="flex-shrink-0 h-10 w-10">
+                        <img
+                          class="h-10 w-10 rounded-full"
+                          src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
+                          alt=""
+                        />
+                      </div>
+                      <div class="ml-4">
+                        <div class="text-sm font-medium text-gray-900">
+                          Someone J
+                        </div>
+                        <div class="text-sm text-gray-500">
+                          someone@gmail.com
+                        </div>
+                      </div>
+                    </div> 
+                  </td>
+                  <td class="px-6 py-4 text-sm text-gray-500 break-all"></td>
+                  <td class="px-6 py-4 text-sm text-gray-500 break-all"></td>
+                  <td class="px-6 py-4 text-sm text-gray-500 break-all"></td>
+                </tr>
+                <tr class="divide-x divide-[#CACACA]"></tr>
+              </tbody> -->
+
+              <!-- if have data -->
               <tbody class="bg-white divide-y divide-[#CACACA]">
                 <tr class="divide-x divide-[#CACACA]">
                   <td class="px-6 py-4 whitespace-nowrap">
