33cfe79ef8862cc20e7ce9d84b5f1e69970c6f5d
Tew2945 <110712512+Tew2945@users.noreply.github.com>
2024-04-24 21:31:32 +0700

    Revert "Merge branch 'sprint1-pbi1-bi'"
    This reverts commit ea124ab652c7eead26fe1bf971e9693c412a7cd7, reversing
    changes made to 4a797763eaa536f911a1c5f2a47751b52142785f.

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import App from '@/App.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'App',
-      component: App
-    },
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: HomeView
+    // },
     // {
     //   path: '/about',
     //   name: 'about',
