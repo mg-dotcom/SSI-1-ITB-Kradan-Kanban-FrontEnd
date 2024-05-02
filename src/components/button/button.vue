aa9f13bf910c8848d11b8e5eaa693785c4b3b9b4
Tew2945 <110712512+Tew2945@users.noreply.github.com>
2024-05-02 15:50:04 +0700

    Update button.vue

--- src/components/button/button.vue
+++ src/components/button/button.vue
@@ -1,20 +1,27 @@
 <script setup>
-import { defineProps } from 'vue';
+import { defineProps } from "vue";
 const props = defineProps({
   buttonType: {
     type: String,
     validator: (value) => {
-      return ['Ok', 'Cancel'].includes(value);
-    }
-  }
+      return ["Ok", "Cancel", "Add"].includes(value);
+    },
+  },
 });
-
 </script>
 
 <template>
- <button :class="{'fancy-btn': true, 'bg-lightgreen': buttonType === 'Ok', 'bg-gray-400': buttonType === 'Cancel'}">
-
-    {{ buttonType === 'Ok' ? 'OK' : 'Cancel' }}
+  <button
+    :class="{
+      'fancy-btn': true,
+      'bg-lightgreen hover:bg-green-500 transition-colors': buttonType === 'Ok',
+      'bg-gray-400 hover:bg-gray-500 transition-colors':
+        buttonType === 'Cancel',
+      'bg-[#03CC0B] hover:bg-green-500 transition-colors shadow-md scale-105 font-bold':
+        buttonType === 'Add',
+    }"
+  >
+    <slot class="">No Status</slot>
   </button>
 </template>
 
@@ -25,6 +32,5 @@ const props = defineProps({
   padding: 5px 10px;
   margin: 5px;
   border-radius: 8px; /* Adding border-radius */
-
 }
 </style>
