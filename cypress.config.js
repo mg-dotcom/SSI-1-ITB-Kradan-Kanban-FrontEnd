e788b5ab3b97f60a3df0c1150d8e92be3e67a9b4
bisbat <baitong.sae-@mail.kmutt.ac.th>
2024-05-05 00:11:46 +0700

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
-    baseUrl: "http://ip23ssi1.sit.kmutt.ac.th:5700",
+    baseUrl: "http://ip23ssi1.sit.kmutt.ac.th",
   },
 });
