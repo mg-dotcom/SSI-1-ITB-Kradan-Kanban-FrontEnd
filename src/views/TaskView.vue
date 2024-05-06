3ed6776a2c2fb73af67c822847bc45ba6cf315fc
mg.apisara <111364885+mg-dotcom@users.noreply.github.com>
2024-05-06 16:10:20 +0700

    fix click delete

--- src/views/TaskView.vue
+++ src/views/TaskView.vue
@@ -337,50 +337,56 @@ const deleteData = async (id) => {
                       >
                         {{ formatStatus(task.status) }}
                       </StatusButton>
-                      <button
-                        class="itbkk-button-action inline-flex items-center p-2 text-sm font-medium text-center text-gray-900 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                        type="button"
-                        @click="showOptionEditDelete(task.id)"
-                      >
-                        <svg
-                          class="w-5 h-5"
-                          aria-hidden="true"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="currentColor"
-                          viewBox="0 0 4 15"
+                      <div>
+                        <div
+                          class="itbkk-button-action inline-flex items-center p-2 text-sm font-medium text-center text-gray-900 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                          type="button"
+                          @click="showOptionEditDelete(task.id)"
                         >
-                          <path
-                            d="M3.5 1.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 6.041a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 5.959a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z"
-                          />
-                        </svg>
-                      </button>
-
-                      <!-- Dropdown menu -->
-                      <div
-                        class="bg-white divide-y divide-gray-100 rounded-lg shadow w-32 dark:bg-gray-700 dark:divide-gray-600 absolute right-[20px]"
-                        v-show="
-                          popup.optionEditDelete && selectedTask.id === task.id
-                        "
-                      >
-                        <ul
-                          class="py-2 text-sm text-gray-700 dark:text-gray-200 z-50"
-                        >
-                          <li class="" @click="editTaskModal(task.id)">
-                            <p
-                              class="itbkk-button-edit block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                          <svg
+                            class="w-5 h-5"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            viewBox="0 0 4 15"
+                          >
+                            <path
+                              d="M3.5 1.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 6.041a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 5.959a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z"
+                            />
+                          </svg>
+                          <div
+                            class="bg-white divide-y divide-gray-100 rounded-lg shadow w-32 dark:bg-gray-700 dark:divide-gray-600 absolute right-[20px]"
+                            v-show="
+                              popup.optionEditDelete &&
+                              selectedTask.id === task.id
+                            "
+                          >
+                            <ul
+                              class="py-2 text-sm text-gray-700 dark:text-gray-200 z-50"
                             >
-                              Edit
-                            </p>
-                          </li>
+                              <li
+                                class="itbkk-button-edit"
+                                @click="editTaskModal(task.id)"
+                              >
+                                <p
+                                  class="itbkk-button-edit block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                                >
+                                  Edit
+                                </p>
+                              </li>
 
-                          <li class="" @click="openDelete(task.id)">
-                            <p
-                              class="itbkk-button-delete block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white text-red-500"
-                            >
-                              Delete
-                            </p>
-                          </li>
-                        </ul>
+                              <li @click="openDelete(task.id)">
+                                <p
+                                  class="itbkk-button-delete block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white text-red-500"
+                                >
+                                  Delete
+                                </p>
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+
+                        <!-- Dropdown menu -->
                       </div>
                     </div>
                   </td>
