99d34ff707576cb5d3979a8390db9436a970259a
bisbat <baitong.sae-@mail.kmutt.ac.th>
2024-08-20 21:20:28 +0700

    user profile and logout icon

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,44 @@
 <script setup></script>
 
 <template>
-  <Toast class="itbkk-message" />
-  <div
-    class="header max-w-full h-[90px] bg-gradient-to-r from-blue to-lightblue"
-  >
-    <img
-      class="object-cover absolute right-0 max-w-max h-[90px]"
-      src="/glass-overlay.png"
-      alt=""
-    />
-    <div class="h-[90px] flex flex-col justify-center p-10">
-      <h1 class="text-header text-white font-bold">IT-Bangmod Kradan kanban</h1>
+    <Toast class="itbkk-message" />
+    <div
+        class="header max-w-full h-[90px] bg-gradient-to-r from-blue to-lightblue"
+    >
+        <img
+            class="object-cover absolute right-0 max-w-max h-[90px]"
+            src="/glass-overlay.png"
+            alt=""
+        />
+        <div class="h-[90px] flex p-10 justify-between items-center">
+            <h1 class="text-header text-white font-bold">
+                IT-Bangmod Kradan kanban
+            </h1>
+            <div class="flex items-center gap-2">
+                <div
+                    class="flex bg-white bg-opacity-45 w-1/7 rounded-full items-center justify-between px-2 py-1"
+                >
+                    <div class="avatar">
+                        <div class="w-10 rounded-full mr-4">
+                            <img
+                                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                            />
+                        </div>
+                    </div>
+                    <div class="text-black font-bold">Somchai Jaidee</div>
+                </div>
+                <div>
+                    <button class="">
+                        <img
+                            src="../assets/logoutIcon.svg"
+                            alt="logout ja bitch"
+                            class="w-8"
+                        />
+                    </button>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <style scoped></style>
