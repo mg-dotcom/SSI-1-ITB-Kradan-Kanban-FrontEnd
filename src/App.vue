a197d9751ecdb85483c50d8f3f6891b210b42710
Tew2945 <110712512+Tew2945@users.noreply.github.com>
2024-09-27 14:27:59 +0700

    update visibility

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <script setup>
 import { RouterView } from "vue-router";
+<<<<<<< Updated upstream
 import AccessDenied from "./views/AccessDenied.vue";
 </script>
 
 <template>
   <!-- <RouterView /> -->
 <AccessDenied />
+=======
+</script>
+
+<template>
+  <RouterView />
+>>>>>>> Stashed changes
 </template>
 
 <style></style>
